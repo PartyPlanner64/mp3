a4864b58940696edc77bdf115cdf718c2dffcc56
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-11-08 12:38:53 -0600

    Match func_800357AC

--- src/thread3_main.c
+++ src/thread3_main.c
@@ -130,4 +130,11 @@ INCLUDE_ASM(void, "thread3_main", func_800354A0);
 //     while (TRUE) {}
 // }
 
-INCLUDE_ASM(s32, "thread3_main", func_800357AC);
+// INCLUDE_ASM(s32, "thread3_main", func_800357AC);
+
+void func_800357AC(s16 count) {
+    s16 i = 0;
+    for (i = 0; i < count; i++) {
+        func_80087A40(*(&D_800A1240[i]), 0, 0x25800); // sizeof(gZBuffer) ?
+    }
+}
