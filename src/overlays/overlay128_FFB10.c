bde3d01f164c8e4d51617e88b1465faab0dac016
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-11-08 18:18:17 -0600

    More splits

---
+++ src/overlays/overlay128_FFB10.c
@@ -0,0 +1,53 @@
+#include "common.h"
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EBEF0_FFB10);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EBF48_FFB68);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EBF98_FFBB8);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EBFE8_FFC08);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC134_FFD54);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC1E8_FFE08);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC39C_FFFBC);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC3C0_FFFE0);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC3E4_100004);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC414_100034);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC4E4_100104);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC590_1001B0);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC5B4_1001D4);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC5EC_10020C);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC628_100248);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC6A8_1002C8);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC6C8_1002E8);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC6EC_10030C);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC70C_10032C);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC8EC_10050C);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC92C_10054C);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC980_1005A0);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800EC9DC_1005FC);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800ECA38_100658);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800ECAA8_1006C8);
+
+INCLUDE_ASM(s32, "overlays/overlay128_FFB10", func_800ECAB4_1006D4);
