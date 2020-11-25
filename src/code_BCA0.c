6398b37da02e419c98685a1be4cd02b3ffc61060
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-11-24 22:44:28 -0600

    BCA0 matches

--- src/code_BCA0.c
+++ src/code_BCA0.c
@@ -1,14 +1,31 @@
 #include "common.h"
 
-INCLUDE_ASM(s32, "code_BCA0", func_8000B0A0);
+extern s32 D_80097650;
 
-INCLUDE_ASM(s32, "code_BCA0", func_8000B0D4);
+s32 func_8000B0A0(s32 arg0, s32 arg1) {
+    return func_8001A150(ReadMainFS(arg0), arg1) & 0xFFFF;
+}
 
-INCLUDE_ASM(s32, "code_BCA0", func_8000B108);
+s32 func_8000B0D4(s32 arg0, s32 arg1) {
+    return func_8001A150(ReadMainFS(arg0), arg1) & 0xFFFF;
+}
 
-INCLUDE_ASM(s32, "code_BCA0", func_8000B13C);
+u32 func_8000B108(s32 arg0, s32 arg1) {
+    return func_8001A150(ReadMainFS(arg0), arg1) & 0xFFFF;
+}
 
-INCLUDE_ASM(s32, "code_BCA0", func_8000B16C);
+s16 func_8000B13C(s32 arg0) {
+    return func_8001A150(ReadMainFS(arg0), 0x1D);
+}
+
+INCLUDE_ASM(u8, "code_BCA0", GetRandomByte);
+// u8 GetRandomByte() {
+//     u32 x;
+
+//     x = D_80097650 * 0x41C64E6D; // FIXME: reg alloc
+//     D_80097650 = x + 0x3039;
+//     return ((x + 0x303A) >> 0x10);
+// }
 
 INCLUDE_ASM(s32, "code_BCA0", func_8000B1A0);
 
