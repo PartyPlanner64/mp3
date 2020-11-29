7f771c9bf69cf2ea89a67f6a09aa2303b5d8bab0
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-11-29 12:58:15 -0600

    Put DecodeLZ back.

--- src/decode.c
+++ src/decode.c
@@ -29,75 +29,75 @@ void DecodeNone(struct decode_struct *decode) {
 extern u8 D_800ABFF0[1024]; // src copy
 extern u8 D_800AC3F0[1024]; // window
 
-//INCLUDE_ASM(s32, "decode", func_8000A2CC);
-void DecodeLZ(struct decode_struct *decode) {
-    u16 flag = 0;
-    u16 windowPos = 958;
-    s32 winTemp;
-    s32 i;
-    s32 byte1;
-    s32 byte2;
-    s32 copyPos;
-    s32 len;
-    s32 copyVal;
-    u8 temp;
+INCLUDE_ASM(s32, "decode", DecodeLZ);
+// void DecodeLZ(struct decode_struct *decode) {
+//     u16 flag = 0;
+//     u16 windowPos = 958;
+//     s32 winTemp;
+//     s32 i;
+//     s32 byte1;
+//     s32 byte2;
+//     s32 copyPos;
+//     s32 len;
+//     s32 copyVal;
+//     u8 temp;
 
-    bzero(&D_800AC3F0, 1024);
+//     bzero(&D_800AC3F0, 1024);
 
-    while (decode->len) {
-        flag = flag >> 1;
-        if ((flag & 0x100) == 0) {
-            if (decode->chunkLen >= 1024) {
-                func_8004DA40(decode->src, &D_800ABFF0, 1024);
-                decode->src += 1024;
-                decode->chunkLen = 0;
-            }
-            flag = 0xFF00 | D_800ABFF0[decode->chunkLen++];
-        }
-        if ((flag & 0x1)) {
-            if (decode->chunkLen >= 1024) {
-                func_8004DA40(decode->src, &D_800ABFF0, 1024);
-                decode->src += 1024;
-                decode->chunkLen = 0;
-            }
+//     while (decode->len) {
+//         flag = flag >> 1;
+//         if ((flag & 0x100) == 0) {
+//             if (decode->chunkLen >= 1024) {
+//                 func_8004DA40(decode->src, &D_800ABFF0, 1024);
+//                 decode->src += 1024;
+//                 decode->chunkLen = 0;
+//             }
+//             flag = 0xFF00 | D_800ABFF0[decode->chunkLen++];
+//         }
+//         if ((flag & 0x1)) {
+//             if (decode->chunkLen >= 1024) {
+//                 func_8004DA40(decode->src, &D_800ABFF0, 1024);
+//                 decode->src += 1024;
+//                 decode->chunkLen = 0;
+//             }
 
-            temp = D_800ABFF0[decode->chunkLen++];
-            winTemp = windowPos++;
-            *(decode->dest++) = temp;
-            D_800AC3F0[winTemp] = temp;
-            windowPos &= 0x3FF;
-            decode->len--;
-        }
-        else {
-            if (decode->chunkLen >= 1024) {
-                func_8004DA40(decode->src, &D_800ABFF0, 1024);
-                decode->src += 1024;
-                decode->chunkLen = 0;
-            }
-            byte1 = D_800ABFF0[decode->chunkLen++];
+//             temp = D_800ABFF0[decode->chunkLen++];
+//             winTemp = windowPos++;
+//             *(decode->dest++) = temp;
+//             D_800AC3F0[winTemp] = temp;
+//             windowPos &= 0x3FF;
+//             decode->len--;
+//         }
+//         else {
+//             if (decode->chunkLen >= 1024) {
+//                 func_8004DA40(decode->src, &D_800ABFF0, 1024);
+//                 decode->src += 1024;
+//                 decode->chunkLen = 0;
+//             }
+//             byte1 = D_800ABFF0[decode->chunkLen++];
 
-            if (decode->chunkLen >= 1024) {
-                func_8004DA40(decode->src, &D_800ABFF0, 1024);
-                decode->src += 1024;
-                decode->chunkLen = 0;
-            }
-            byte2 = D_800ABFF0[decode->chunkLen++];
+//             if (decode->chunkLen >= 1024) {
+//                 func_8004DA40(decode->src, &D_800ABFF0, 1024);
+//                 decode->src += 1024;
+//                 decode->chunkLen = 0;
+//             }
+//             byte2 = D_800ABFF0[decode->chunkLen++];
 
-            copyPos = byte1 | ((byte2 & 0xC0) << 2);
-            len = 3 + (byte2 & 0x3F);
+//             copyPos = byte1 | ((byte2 & 0xC0) << 2);
+//             len = 3 + (byte2 & 0x3F);
 
-            for (i = 0; i < len; i++) {
-                {
-                    winTemp = windowPos++;
-                    *(decode->dest++) = (copyVal = D_800AC3F0[(copyPos + i) & 0x3FF]);
-                    D_800AC3F0[winTemp] = copyVal;
-                    windowPos &= 0x3FF;
-                }
-            }
-            decode->len -= len;
-        }
-    }
-}
+//             for (i = 0; i < len; i++) {
+//                 {
+//                     winTemp = windowPos++;
+//                     *(decode->dest++) = (copyVal = D_800AC3F0[(copyPos + i) & 0x3FF]);
+//                     D_800AC3F0[winTemp] = copyVal;
+//                     windowPos &= 0x3FF;
+//                 }
+//             }
+//             decode->len -= len;
+//         }
+//     }
+// }
 
 INCLUDE_ASM(s32, "decode", func_8000A53C);
 
