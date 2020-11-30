7fe30aeec87db9f339bb1e4d691c73f53738db9b
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-11-29 23:03:52 -0600

    Match func_8000A53C

--- src/decode.c
+++ src/decode.c
@@ -103,152 +103,150 @@ INCLUDE_ASM(s32, "decode", DecodeLZ);
 //     }
 // }
 
-INCLUDE_ASM(s32, "decode", func_8000A53C);
-// void func_8000A53C(struct decode_struct *decode) {
-//     s32 codeWordBitsRemaining;
-//     s32 curCodeWord;
-//     u32 back, back2;
-//     s32 backShift, count;
-//     u8 *ptr;
-//     u8 *destOrig;
+void func_8000A53C(struct decode_struct *decode) {
+    s32 codeWordBitsRemaining;
+    s32 curCodeWord;
+    u32 back, back2, count;
+    u8 *ptr;
+    u8 *destOrig;
 
-//     // Advance past the first 4 bytes.
-//     if (decode->chunkLen >= 1024) {
-//         func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//         decode->src += 1024;
-//         decode->chunkLen = 0;
-//     }
-//     decode->chunkLen++;
-//     if (decode->chunkLen >= 1024) {
-//         func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//         decode->src += 1024;
-//         decode->chunkLen = 0;
-//     }
-//     decode->chunkLen++;
-//     if (decode->chunkLen >= 1024) {
-//         func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//         decode->src += 1024;
-//         decode->chunkLen = 0;
-//     }
-//     decode->chunkLen++;
-//     if (decode->chunkLen >= 1024) {
-//         func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//         decode->src += 1024;
-//         decode->chunkLen = 0;
-//     }
-//     decode->chunkLen++;
+    // Advance past the first 4 bytes.
+    if (decode->chunkLen >= 1024) {
+        func_8004DA40(decode->src, &D_800ABFF0, 1024);
+        decode->src += 1024;
+        decode->chunkLen = 0;
+    }
+    decode->chunkLen++;
+    if (decode->chunkLen >= 1024) {
+        func_8004DA40(decode->src, &D_800ABFF0, 1024);
+        decode->src += 1024;
+        decode->chunkLen = 0;
+    }
+    decode->chunkLen++;
+    if (decode->chunkLen >= 1024) {
+        func_8004DA40(decode->src, &D_800ABFF0, 1024);
+        decode->src += 1024;
+        decode->chunkLen = 0;
+    }
+    decode->chunkLen++;
+    if (decode->chunkLen >= 1024) {
+        func_8004DA40(decode->src, &D_800ABFF0, 1024);
+        decode->src += 1024;
+        decode->chunkLen = 0;
+    }
+    decode->chunkLen++;
 
-//     curCodeWord = 0;
-//     codeWordBitsRemaining = 0;
-//     destOrig = decode->dest;
+    codeWordBitsRemaining = 0;
+    curCodeWord = 0;
+    destOrig = decode->dest;
 
-//     while (decode->len != 0) {
-//         // Read a new code word.
-//         if (codeWordBitsRemaining == 0) {
-//             {
-//                 u16 chunkByte1, chunkByte2, chunkByte3, chunkByte4;
+    while (decode->len != 0) {
+        // Read a new code word.
+        if (codeWordBitsRemaining == 0) {
+            {
+                u32 chunkByte1, chunkByte2, chunkByte3, chunkByte4;
 
-//                 if (decode->chunkLen >= 1024) {
-//                     func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//                     decode->src += 1024;
-//                     decode->chunkLen = 0;
-//                 }
-//                 chunkByte1 = D_800ABFF0[decode->chunkLen++];
+                if (decode->chunkLen >= 1024) {
+                    func_8004DA40(decode->src, &D_800ABFF0, 1024);
+                    decode->src += 1024;
+                    decode->chunkLen = 0;
+                }
+                chunkByte1 = D_800ABFF0[decode->chunkLen++];
 
-//                 if (decode->chunkLen >= 1024) {
-//                     func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//                     decode->src += 1024;
-//                     decode->chunkLen = 0;
-//                 }
-//                 chunkByte2 = D_800ABFF0[decode->chunkLen++];
+                if (decode->chunkLen >= 1024) {
+                    func_8004DA40(decode->src, &D_800ABFF0, 1024);
+                    decode->src += 1024;
+                    decode->chunkLen = 0;
+                }
+                chunkByte2 = D_800ABFF0[decode->chunkLen++];
 
-//                 if (decode->chunkLen >= 1024) {
-//                     func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//                     decode->src += 1024;
-//                     decode->chunkLen = 0;
-//                 }
-//                 chunkByte3 = D_800ABFF0[decode->chunkLen++];
+                if (decode->chunkLen >= 1024) {
+                    func_8004DA40(decode->src, &D_800ABFF0, 1024);
+                    decode->src += 1024;
+                    decode->chunkLen = 0;
+                }
+                chunkByte3 = D_800ABFF0[decode->chunkLen++];
 
-//                 if (decode->chunkLen >= 1024) {
-//                     func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//                     decode->src += 1024;
-//                     decode->chunkLen = 0;
-//                 }
-//                 chunkByte4 = D_800ABFF0[decode->chunkLen++];
+                if (decode->chunkLen >= 1024) {
+                    func_8004DA40(decode->src, &D_800ABFF0, 1024);
+                    decode->src += 1024;
+                    decode->chunkLen = 0;
+                }
+                chunkByte4 = D_800ABFF0[decode->chunkLen++];
 
-//                 curCodeWord = (chunkByte1 << 24) + (chunkByte2 << 16) + (chunkByte3 << 8) + chunkByte4;
-//                 codeWordBitsRemaining = 32;
-//             }
-//         }
+                curCodeWord = (chunkByte1 << 24) + (chunkByte2 << 16) + (chunkByte3 << 8) + chunkByte4;
+                codeWordBitsRemaining = 32;
+            }
+        }
 
-//         if (curCodeWord < 0) {
-//             {
-//                 u16 nextByte;
+        if (curCodeWord < 0) {
+            {
+                u32 nextByte;
 
-//                 // Copy the next byte from the source to the destination.
-//                 if (decode->chunkLen >= 1024) {
-//                     func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//                     decode->src += 1024;
-//                     decode->chunkLen = 0;
-//                 }
-//                 nextByte = D_800ABFF0[decode->chunkLen++];
+                // Copy the next byte from the source to the destination.
+                if (decode->chunkLen >= 1024) {
+                    func_8004DA40(decode->src, &D_800ABFF0, 1024);
+                    decode->src += 1024;
+                    decode->chunkLen = 0;
+                }
+                nextByte = D_800ABFF0[decode->chunkLen++];
 
-//                 *(decode->dest++) = nextByte;
-//                 decode->len--;
-//             }
-//         }
-//         else {
-//             // Interpret the next two bytes as a distance to travel backwards and a
-//             // a length to read.
-//             if (decode->chunkLen >= 1024) {
-//                 func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//                 decode->src += 1024;
-//                 decode->chunkLen = 0;
-//             }
-//             back = D_800ABFF0[decode->chunkLen++];
+                *(decode->dest++) = nextByte;
+                decode->len--;
+            }
+        }
+        else {
+            // Interpret the next two bytes as a distance to travel backwards and a
+            // a length to read.
+            if (decode->chunkLen >= 1024) {
+                func_8004DA40(decode->src, &D_800ABFF0, 1024);
+                decode->src += 1024;
+                decode->chunkLen = 0;
+            }
+            back = D_800ABFF0[decode->chunkLen++];
 
-//             if (decode->chunkLen >= 1024) {
-//                 func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//                 decode->src += 1024;
-//                 decode->chunkLen = 0;
-//             }
-//             back2 = D_800ABFF0[decode->chunkLen++];
+            if (decode->chunkLen >= 1024) {
+                func_8004DA40(decode->src, &D_800ABFF0, 1024);
+                decode->src += 1024;
+                decode->chunkLen = 0;
+            }
+            back2 = D_800ABFF0[decode->chunkLen++];
 
-//             back <<= 8;
-//             back += back2;
-//             backShift = back >> 12;
-//             back &= 0xFFF;
-//             ptr = decode->dest - back;
-//             if (backShift == 0) {
-//                 if (decode->chunkLen >= 1024) {
-//                     func_8004DA40(decode->src, &D_800ABFF0, 1024);
-//                     decode->src += 1024;
-//                     decode->chunkLen = 0;
-//                 }
+            back2 = (back << 8) + (back2 & 0xFFFFFFFFFFFFFFFF); // Unlikely, but matching.
+            count = (back << 8) >> 12;
+            back2 &= 0xFFF;
+            ptr = decode->dest - back2;
+            if (count == 0) {
+                if (decode->chunkLen >= 1024) {
+                    func_8004DA40(decode->src, &D_800ABFF0, 1024);
+                    decode->src += 1024;
+                    decode->chunkLen = 0;
+                }
 
-//                 count = D_800ABFF0[decode->chunkLen++] + 0x12;
-//             }
-//             else {
-//                 count = backShift + 2;
-//             }
-//             decode->len -= count;
+                count = 0x12;
+                count += D_800ABFF0[decode->chunkLen++];
+            }
+            else {
+                count += 2;
+            }
+            decode->len -= count;
 
-//             while (count != 0) {
-//                 if (ptr - 1 < destOrig) {
-//                     *(decode->dest++) = 0;
-//                 }
-//                 else {
-//                     *(decode->dest++) = *(ptr - 1);
-//                 }
-//                 count--;
-//                 ptr++;
-//             }
-//         }
+            while (count != 0) {
+                if (ptr - 1 < destOrig) {
+                    *(decode->dest++) = 0;
+                }
+                else {
+                    *(decode->dest++) = *(ptr - 1);
+                }
+                count--;
+                ptr++;
+            }
+        }
 
-//         curCodeWord <<= 1;
-//         codeWordBitsRemaining--;
-//     }
-// }
+        curCodeWord <<= 1;
+        codeWordBitsRemaining--;
+    }
+}
 
 INCLUDE_ASM(s32, "decode", func_8000A9E8);
 
