6214cddbaff80c19322c24710b3109816d21e8af
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-12-01 21:46:51 -0600

    Break out EFC0

---
+++ src/code_EFC0.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM(s32, "code_EFC0", func_8000E3C0);
+
+INCLUDE_ASM(s32, "code_EFC0", func_8000E740);
+
+INCLUDE_ASM(s32, "code_EFC0", func_8000E78C);
+
+INCLUDE_ASM(s32, "code_EFC0", func_8000E7B8);
+
+INCLUDE_ASM(s32, "code_EFC0", func_8000E804);
+
+INCLUDE_ASM(s32, "code_EFC0", func_8000E978);
