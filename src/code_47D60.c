15842d54c777f947a77c5c0497c4fc4abffd5fd6
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-12-08 20:06:30 -0600

    InitObjSys

--- src/code_47D60.c
+++ src/code_47D60.c
@@ -1,7 +1,10 @@
 #include "common.h"
 #include "process.h"
+#include "object.h"
+#include "heap_temporary.h"
 
 extern s8 D_800A1762;
+extern struct object_indirect *D_800A1770;
 extern s16 D_800A1774;
 extern s16 D_800A1776;
 extern s16 D_800A1778;
@@ -12,16 +15,128 @@ struct str800A177C {
     struct process *process;
     s32 unk8;
 };
-extern struct str800A177C *D_800A177C[];
+extern struct str800A177C *D_800A177C;
 
 extern s16 D_800B23C0;
 
+extern f32 D_800CB890;
+extern s16 D_800CC430;
+extern s16 D_800CCF50;
+extern s16 D_800CCF8C;
+extern f32 D_800CCF98[];
 extern s16 D_800CD050;
+
+struct strCD1E4 {
+    s16 unk0;
+    s16 unk2;
+    s16 unk4;
+    s32 unk8;
+    s32 unkC;
+};
+extern struct strCD1E4 D_800CD1E0[];
+
 extern s8 D_800CD280;
+
+extern s32 D_800D2118;
+extern f32 D_800D4198[];
+extern f32 D_800D5210;
+extern s16 D_800D530C;
+extern s16 D_800D5560;
+extern s32 D_800D6A40;
+extern s8 D_800D6A90;
+extern s16 D_800D6B40;
 
 extern void func_80048054();
 
-INCLUDE_ASM(s32, "code_47D60", func_80047160);
+// InitObjSys
+void func_80047160(s32 arg0, s16 arg1) {
+    s32 i;
+
+    D_800CCF50 = arg0 + 1;
+    D_800CCF8C = 0;
+    D_800D6B40 = 0;
+    D_800CC430 = -1;
+    D_800D5560 = -1;
+    D_800D2118 = 0;
+    D_800D530C = 0;
+    D_800D6A90 = 0;
+
+    {
+        void *ptr = D_800A1770;
+        if (ptr != NULL) {
+            FreeTemp(ptr);
+        }
+    }
+
+    {
+        f32 zero;
+        struct object_indirect *objind;
+
+        D_800A1770 = MallocTemp(D_800CCF50 * sizeof(struct object_indirect));
+
+        for (i = 0; i < D_800CCF50; i++) {
+            objind = &D_800A1770[i];
+            objind->unk0 = 1;
+            objind->unk8 = -1;
+            objind->unk6 = -1;
+            objind->unk4 = -1;
+            objind->unk10 = 0;
+            zero = 0.0f;
+            objind->unk2C = zero;
+            objind->unk28 = 0.0f;
+            objind->unk24 = 0.0f;
+            objind->unk20 = 0.0f;
+            objind->unk1C = 0.0f;
+            objind->unk18 = 0.0f;
+            objind->unk38 = 1.0f;
+            objind->unk34 = 1.0f;
+            objind->unk30 = 1.0f;
+            objind->unk48 = NULL;
+            objind->unk40 = NULL;
+            objind->unk5C = NULL;
+            objind->unk14 = NULL;
+            objind->unkA = i + 1;
+            objind->unk44 = 0;
+            objind->unk48 = NULL;
+        }
+    }
+
+    for (i = 0; i < 10; i++) {
+        D_800CD1E0[i].unk2 = 0;
+        D_800CD1E0[i].unk4 = 0;
+        D_800CD1E0[i].unk0 = 0;
+        D_800CD1E0[i].unkC = 0;
+        D_800CD1E0[i].unk8 = 0;
+    }
+
+    D_800A1774 = arg1 + 2;
+    D_800A1776 = 0;
+    D_800A1778 = 0;
+    D_800A177C = MallocTemp(D_800A1774 * sizeof(struct str800A177C));
+
+    {
+        struct str800A177C *str;
+
+        for (i = 0; i < D_800A1774; i++) {
+            str = &D_800A177C[i];
+            str->unk0 = 1;
+            str->unk2 = i + 1;
+            str->process = NULL;
+            str->unk8 = 0;
+        }
+    }
+
+    D_800D5210 = 20000.0f;
+    D_800CB890 = 10000.0f;
+
+    for (i = 0; i < 4; i++) {
+        D_800CCF98[i] = 20000.0f;
+        D_800D4198[i] = 10000.0f;
+    }
+
+    D_800D6A40 = 0;
+    D_800CD050 = (u16)0x5A;
+}
 
 INCLUDE_ASM(s32, "code_47D60", func_80047420);
 
@@ -76,7 +191,7 @@ struct process *InitProcess(void *func, u16 priority, s32 stackSize, s32 extraDa
     if (D_800A1776 != D_800A1774) {
         temp_v0 = D_800A1778;
         dtorIndex = temp_v0;
-        temp_s0 = &(*D_800A177C)[temp_v0];
+        temp_s0 = &D_800A177C[temp_v0];
         temp_s0->unk0 = 4;
         D_800A1778 = temp_s0->unk2;
         process = CreateProcess(func, priority, stackSize, extraDataSize);
