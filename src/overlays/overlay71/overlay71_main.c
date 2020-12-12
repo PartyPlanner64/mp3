ed1959b90ac78b23932d7974fdeedaa1205573a4
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-12-11 22:57:15 -0600

    overlay71_main

--- src/overlays/overlay71/overlay71_main.c
+++ src/overlays/overlay71/overlay71_main.c
@@ -1,3 +1,19 @@
 #include "common.h"
+#include "../../board.h"
+#include "../../code_365E0.h"
 
-INCLUDE_ASM(s32, "overlays/overlay71/overlay71_main", func_801059A0_31B3B0);
+extern s16 D_800A1764;
+extern void func_801059D0_31B3E0();
+extern void func_80105A44_31B454();
+extern void func_80105A98_31B4A8();
+
+struct overlay_entrypoint overlay71_entrypoints[] = {
+    { 0, func_801059D0_31B3E0 },
+    { 1, func_80105A44_31B454 },
+    { 2, func_80105A98_31B4A8 },
+    { -1, NULL }
+};
+
+void func_801059A0_31B3B0() {
+    func_800359E0(overlay71_entrypoints, D_800A1764);
+}
