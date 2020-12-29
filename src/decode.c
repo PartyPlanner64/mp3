ffc21fa22f4e884caadc2cec318145e2bf2eebb7
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-12-29 14:57:08 -0600

    Match decode.c rodata.

--- src/decode.c
+++ src/decode.c
@@ -450,39 +450,37 @@ void func_8000AE64(struct decode_struct *decode) {
     }
 }
 
-INCLUDE_ASM(s32, "decode", DecodeFile);
-// Correct except for jump table data.
-// void DecodeFile(void *src, void *dest, s32 len, s32 decodeType)
-// {
-//     struct decode_struct decodeStruct;
-//     struct decode_struct *decodePtr = &decodeStruct;
-//     decodeStruct.src = (u8 *)src;
-//     decodeStruct.dest = (u8 *)dest;
-//     decodeStruct.len = len;
-//     decodeStruct.chunkLen = 1024;
-//     switch (decodeType) {
-//         case 0:
-//             DecodeNone(decodePtr);
-//             break;
+void DecodeFile(void *src, void *dest, s32 len, s32 decodeType)
+{
+    struct decode_struct decodeStruct;
+    struct decode_struct *decodePtr = &decodeStruct;
+    decodeStruct.src = (u8 *)src;
+    decodeStruct.dest = (u8 *)dest;
+    decodeStruct.len = len;
+    decodeStruct.chunkLen = 1024;
+    switch (decodeType) {
+        case 0:
+            DecodeNone(decodePtr);
+            break;
 
-//         case 1:
-//             func_8000A2CC(decodePtr); // DecodeLZ
-//             break;
+        case 1:
+            DecodeLZ(decodePtr);
+            break;
 
-//         case 2:
-//             func_8000A53C(decodePtr);
-//             break;
+        case 2:
+            func_8000A53C(decodePtr);
+            break;
 
-//         case 3:
-//         case 4:
-//             func_8000A9E8(decodePtr);
-//             break;
+        case 3:
+        case 4:
+            func_8000A9E8(decodePtr);
+            break;
 
-//         case 5:
-//             func_8000AE64(decodePtr);
-//             break;
+        case 5:
+            func_8000AE64(decodePtr);
+            break;
 
-//         default:
-//             break;
-//     }
-// }
+        default:
+            break;
+    }
+}
