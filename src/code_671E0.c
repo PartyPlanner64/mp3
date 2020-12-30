826ccf157e58b5c5743fb18e44b49556da9d0bef
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-12-30 00:28:36 -0600

    Split out HVQ decoder .c file.

---
+++ src/code_671E0.c
@@ -0,0 +1,24 @@
+#include "common.h"
+
+// HVQ decoder library.
+
+INCLUDE_ASM(s32, "code_671E0", func_800665E0);
+
+INCLUDE_ASM(s32, "code_671E0", func_80066894);
+
+INCLUDE_ASM(s32, "code_671E0", func_80066A44);
+
+INCLUDE_ASM(s32, "code_671E0", func_800677B8);
+
+INCLUDE_ASM(s32, "code_671E0", func_80067D50);
+
+INCLUDE_ASM(s32, "code_671E0", func_80068ECC);
+
+INCLUDE_ASM(s32, "code_671E0", func_800694D0);
+
+// Decodes an HVQS
+INCLUDE_ASM(void, "code_671E0", func_800698E8, void *hvqsData, s32 arg1, s32 arg2, void *arg3);
+
+INCLUDE_ASM(s32, "code_671E0", func_80069E68);
+
+INCLUDE_ASM(s32, "code_671E0", func_8006A370);
