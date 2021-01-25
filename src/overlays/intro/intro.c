382b5e95fcdddbc91e5f3ce539b592bdd0f43b0d
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-01-25 08:17:07 -0600

    func_80105ACC_3D727C

--- src/overlays/intro/intro.c
+++ src/overlays/intro/intro.c
@@ -1,15 +1,20 @@
 #include "common.h"
 
-extern void *D_80105F00_22E660;
+extern s32 D_80105F00_22E660;
 
 INCLUDE_ASM(void, "overlays/intro/intro", D_801059D0_3D7180);
 
+// entrypoint 0
 void func_80105AAC_3D725C() {
     D_80105F00_22E660 = 0;
     D_801059D0_3D7180();
 }
 
-INCLUDE_ASM(s32, "overlays/intro/intro", func_80105ACC_3D727C);
+// entrypoint 1
+void func_80105ACC_3D727C() {
+    D_80105F00_22E660 = 1;
+    D_801059D0();
+}
 
 INCLUDE_ASM(s32, "overlays/intro/intro", D_80105AF0_3D72A0);
 
