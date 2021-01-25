1f4e1336250eaab05721a68b531d82bf3b98712d
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-01-25 08:18:13 -0600

    D_80105BA4_3D7354

--- src/overlays/intro/intro.c
+++ src/overlays/intro/intro.c
@@ -18,7 +18,17 @@ void func_80105ACC_3D727C() {
 
 INCLUDE_ASM(s32, "overlays/intro/intro", D_80105AF0_3D72A0);
 
-INCLUDE_ASM(s32, "overlays/intro/intro", D_80105BA4_3D7354);
+void D_80105BA4_3D7354() {
+    if (func_800620BC() == 0) {
+        func_8005F524();
+        if (D_80105F00_22E660 != 0) {
+            func_80048228(0x7A, 2, 0x92);
+            return;
+        }
+        func_80048128(0x7A, 2, 0x92);
+        func_80048460(1, 0x7A, 2, 0x92);
+    }
+}
 
 INCLUDE_ASM(s32, "overlays/intro/intro", D_80105C14_3D73C4);
 
