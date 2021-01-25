776b0bb68830c75a6dc2e1bec353ba6f9d6d5e0b
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-01-24 21:45:18 -0600

    Break out intro overlay

---
+++ src/overlays/intro/intro.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+INCLUDE_ASM(s32, "overlays/intro/intro", D_801059D0_3D7180);
+
+INCLUDE_ASM(s32, "overlays/intro/intro", func_80105AAC_3D725C);
+
+INCLUDE_ASM(s32, "overlays/intro/intro", func_80105ACC_3D727C);
+
+INCLUDE_ASM(s32, "overlays/intro/intro", D_80105AF0_3D72A0);
+
+INCLUDE_ASM(s32, "overlays/intro/intro", D_80105BA4_3D7354);
+
+INCLUDE_ASM(s32, "overlays/intro/intro", D_80105C14_3D73C4);
+
+INCLUDE_ASM(s32, "overlays/intro/intro", D_80105C80_3D7430);
