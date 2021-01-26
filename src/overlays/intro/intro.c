fe766e3b63890b11ddf0016a12e1164f16413a56
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-01-25 21:29:33 -0600

    D_80105C80_3D7430

--- src/overlays/intro/intro.c
+++ src/overlays/intro/intro.c
@@ -1,5 +1,14 @@
 #include "common.h"
 
+extern s16 func_8000B838(s32);
+extern s32 func_8000BFEC(s16, s32, s32);
+extern void func_8000BBD4(u16, s32, s32);
+extern void func_8000BB54(u16);
+extern void func_8000BCC8(u16, s32);
+extern void func_8000C184(u16);
+
+extern u16 D_800D530C;
+
 extern s32 D_80105F00_22E660;
 
 INCLUDE_ASM(void, "overlays/intro/intro", D_801059D0_3D7180);
@@ -32,4 +41,68 @@ void D_80105BA4_3D7354() {
 
 INCLUDE_ASM(s32, "overlays/intro/intro", D_80105C14_3D73C4);
 
-INCLUDE_ASM(s32, "overlays/intro/intro", D_80105C80_3D7430);
+void D_80105C80_3D7430() {
+    s16 temp_s2;
+    s32 temp_s1;
+
+    temp_s2 = func_8000B838(0x110000);
+    temp_s1 = func_8000BFEC(temp_s2, 0, 1);
+    func_8000BBD4(temp_s1, 0xA0, 0x78);
+    func_8000BB54(temp_s1);
+    func_8000BCC8(temp_s1, 0xFFFF);
+    func_80061FE8(0xB, 0x1E);
+
+    while (func_800620BC() != 0) {
+        SleepVProcess();
+    }
+    SleepProcess(0x25);
+
+    func_80062050(0xB, 9);
+
+    while (func_800620BC() != 0) {
+        SleepVProcess();
+    }
+
+    func_8000C184(temp_s1);
+    func_80055670(temp_s2);
+    SleepProcess(9);
+
+    temp_s2 = func_8000B838(0x110001);
+    temp_s1 = func_8000BFEC(temp_s2, 0, 1);
+    func_8000BBD4(temp_s1, 0xA0, 0x78);
+    func_8000BB54(temp_s1);
+    func_8000BCC8(temp_s1, 0xFFFF);
+    func_80061FE8(0xB, 9);
+
+    while (func_800620BC() != 0) {
+        SleepVProcess();
+    }
+    SleepProcess(0x25);
+
+    func_80062050(0xB, 9);
+
+    while (func_800620BC() != 0) {
+        SleepVProcess();
+    }
+
+    func_8000C184(temp_s1);
+    func_80055670(temp_s2);
+    SleepProcess(9);
+
+    temp_s2 = func_8000B838(0x110002);
+    temp_s1 = func_8000BFEC(temp_s2, 0, 1);
+    func_8000BBD4(temp_s1, 0xA0, 0x78);
+    func_8000BB54(temp_s1);
+    func_8000BCC8(temp_s1, 0xFFFF);
+    func_80061FE8(0xB, 9);
+
+    while (func_800620BC() != 0) {
+        SleepVProcess();
+    }
+    SleepProcess(0x25);
+    D_800D530C = 1;
+
+    while (1) {
+        SleepVProcess();
+    }
+}
