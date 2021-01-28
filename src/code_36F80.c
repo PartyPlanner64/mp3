eb46641419955d3c9aef7a90bd90937a9157c730
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-01-27 21:55:25 -0600

    func_80036448

--- src/code_36F80.c
+++ src/code_36F80.c
@@ -2,12 +2,12 @@
 
 extern void *D_800B1A40; // strings ROM pointer
 extern s32 D_800B1A44; // string directory count
-extern void *D_800B1A48; // string directory table
+extern u32 *D_800B1A48; // string directory table
 extern s32 D_800B1A4C; // string directory table size
 
 void func_80036380(void *stringsRomPtr) {
     s32 stringDirTableSize;
-    void *stringDirTable;
+    u32 *stringDirTable;
     u32 *stringsHeader;
 
     D_800B1A40 = stringsRomPtr;
@@ -27,7 +27,30 @@ void func_80036414(void *ptr) {
     func_8004DA40(ptr + 4, D_800B1A48, D_800B1A4C);
 }
 
-INCLUDE_ASM(s32, "code_36F80", func_80036448);
+struct str80036448 {
+    void *unk0;
+    s32 unk4; // decompressed size
+    s32 unk8; // compression type
+};
+
+struct string_dir_header {
+    s32 decompressedSize;
+    s32 compressionType;
+};
+
+void func_80036448(s32 arg0, struct str80036448 *arg1) {
+    s32 temp_a0;
+    struct string_dir_header *dirHeader;
+
+    dirHeader = MallocPerm(16);
+    temp_a0 = ((s8 *)D_800B1A40) + *(D_800B1A48 + arg0);
+    arg1->unk0 = temp_a0;
+    func_8004DA40(temp_a0, dirHeader, 16);
+    arg1->unk0 = arg1->unk0 + 8;
+    arg1->unk4 = dirHeader->decompressedSize;
+    arg1->unk8 = dirHeader->compressionType;
+    FreePerm(dirHeader);
+}
 
 INCLUDE_ASM(s32, "code_36F80", func_800364DC);
 
