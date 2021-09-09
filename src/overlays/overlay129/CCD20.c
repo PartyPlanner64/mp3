687ccd72edaf6119ed3b82f653eff836ddfe5f8f
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-09-08 21:59:44 -0500

    CCD20 hvq rom pointer

---
+++ src/overlays/overlay129/CCD20.c
@@ -0,0 +1,167 @@
+#include "common.h"
+
+extern void func_800E4F50_CCD20(u8 *);
+
+INCLUDE_ASM(void, "overlays/overlay129/CCD20", func_800E4F50_CCD20, u8 *hvqPtr);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E4FF4_CCDC4);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5000_CCDD0);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5040_CCE10);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E52DC_CD0AC);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E52F8_CD0C8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5360_CD130);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5374_CD144);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E53B0_CD180);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E53E8_CD1B8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5424_CD1F4);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E545C_CD22C);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E55E4_CD3B4);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5618_CD3E8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5690_CD460);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5784_CD554);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5860_CD630);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5954_CD724);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5964_CD734);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5974_CD744);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E59B0_CD780);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5A00_CD7D0);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5AC8_CD898);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5B3C_CD90C);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5B80_CD950);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5BE8_CD9B8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5C20_CD9F0);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5C58_CDA28);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5C70_CDA40);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5C94_CDA64);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5CC4_CDA94);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5CEC_CDABC);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5D18_CDAE8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5DD8_CDBA8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E5FB0_CDD80);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E77D8_CF5A8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E7C04_CF9D4);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E7C10_CF9E0);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E7C1C_CF9EC);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E7D18_CFAE8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E7D90_CFB60);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E7E80_CFC50);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E7EF8_CFCC8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8110_CFEE0);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8164_CFF34);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8180_CFF50);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8194_CFF64);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E856C_D033C);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8578_D0348);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8584_D0354);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8648_D0418);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E86B8_D0488);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8704_D04D4);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E875C_D052C);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8870_D0640);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E887C_D064C);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E88B0_D0680);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E88E4_D06B4);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E88F0_D06C0);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E88FC_D06CC);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E890C_D06DC);
+
+void func_800E89CC_D079C() {
+    func_800E4F50_CCD20(&bin_hvq_ROM_START);
+}
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E89EC_D07BC);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8A1C_D07EC);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8A44_D0814);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8AB0_D0880);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8D10_D0AE0);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8D54_D0B24);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E8EE8_D0CB8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E9030_D0E00);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E90B4_D0E84);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E90F4_D0EC4);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E9108_D0ED8);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E911C_D0EEC);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E9158_D0F28);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E9180_D0F50);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E9190_D0F60);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E919C_D0F6C);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E9200_D0FD0);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E9230_D1000);
+
+INCLUDE_ASM(s32, "overlays/overlay129/CCD20", func_800E924C_D101C);
