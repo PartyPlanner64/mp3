7bd86328a54047dafacf40197ab16f5124bcc72d
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-09-10 16:28:08 -0500

    Split out 4F01F0

---
+++ src/overlays/overlay115/4F01F0.c
@@ -0,0 +1,32 @@
+#include "common.h"
+
+// References hvq rom start
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_801059A0_4F01F0);
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_80105E90_4F06E0);
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_80105FD4_4F0824);
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_80106054_4F08A4);
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_80106080_4F08D0);
+
+void func_80106270_4F0AC0(void) {
+}
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_80106278_4F0AC8);
+
+void func_80106544_4F0D94(void) {
+}
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_8010654C_4F0D9C);
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_80106560_4F0DB0);
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_80106574_4F0DC4);
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_80106730_4F0F80);
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_80106744_4F0F94);
+
+INCLUDE_ASM(s32, "overlays/overlay115/4F01F0", func_80106850_4F10A0);
