c79a940cd71a28d7ecf87c7c4eb67983563a9347
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-09-10 17:01:25 -0500

    Patch nonmatching ROM string references

--- tools/patches.py
+++ tools/patches.py
@@ -19,3 +19,29 @@ for file_path in hvq_asm_files:
 
     with open(file_path, 'w') as file:
         file.write(filedata)
+
+# Nonmatching String ROM references are in a single tricky function.
+strings_fn = "asm/nonmatchings/5ACF0/func_8005A6B0.s"
+with open(strings_fn, 'r') as file :
+    filedata = file.read()
+
+filedata = filedata.replace('$a0, 0x121', '$a0, %hi(bin_strings_jp_ROM_START)')
+filedata = filedata.replace('$a0, $a0, -0x67b0', '$a0, $a0, %lo(bin_strings_jp_ROM_START)')
+
+filedata = filedata.replace('$a0, 0x122', '$a0, %hi(bin_strings_en_ROM_START)')
+filedata = filedata.replace('$a0, $a0, -0x3560', '$a0, $a0, %lo(bin_strings_en_ROM_START)')
+
+filedata = filedata.replace('$a0, 0x125', '$a0, %hi(bin_strings_fr_ROM_START)')
+filedata = filedata.replace('$a0, $a0, -0x2bc0', '$a0, $a0, %lo(bin_strings_fr_ROM_START)')
+
+filedata = filedata.replace('$a0, 0x123', '$a0, %hi(bin_strings_de_ROM_START)')
+filedata = filedata.replace('$a0, $a0, 0x55c0', '$a0, $a0, %lo(bin_strings_de_ROM_START)')
+
+filedata = filedata.replace('$a0, 0x127', '$a0, %hi(bin_strings_es_ROM_START)')
+filedata = filedata.replace('$a0, $a0, 0x65f0', '$a0, $a0, %lo(bin_strings_es_ROM_START)')
+
+filedata = filedata.replace('$a0, 0x126', '$a0, %hi(bin_strings_it_ROM_START)')
+filedata = filedata.replace('$a0, $a0, 0x1f90', '$a0, $a0, %lo(bin_strings_it_ROM_START)')
+
+with open(strings_fn, 'w') as file:
+    file.write(filedata)
