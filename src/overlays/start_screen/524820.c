56022b3516f1b711417656e91b4c7cc13108b73e
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-09-10 16:09:10 -0500

    func_801080A8_5266D8

---
+++ src/overlays/start_screen/524820.c
@@ -0,0 +1,306 @@
+#include "common.h"
+
+extern void *D_80114BB8_5331E8;
+extern void *D_80114BC4_5331F4;
+extern void *D_80114BD0_533200;
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801061F0_524820);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_8010621C_52484C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801062B0_5248E0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801062C4_5248F4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801062E0_524910);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106318_524948);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106370_5249A0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801063D8_524A08);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106448_524A78);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_8010647C_524AAC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801064C0_524AF0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801064EC_524B1C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106524_524B54);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106554_524B84);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106584_524BB4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801065A0_524BD0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801065BC_524BEC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106614_524C44);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106644_524C74);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106694_524CC4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801066CC_524CFC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106700_524D30);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106734_524D64);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106768_524D98);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801067D4_524E04);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106820_524E50);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801068BC_524EEC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106944_524F74);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801069F4_525024);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106A2C_52505C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106A50_525080);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106A74_5250A4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106ADC_52510C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106B50_525180);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106BB8_5251E8);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106C0C_52523C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106C54_525284);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106C9C_5252CC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106CDC_52530C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106D1C_52534C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106D60_525390);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106DD0_525400);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106E14_525444);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106E4C_52547C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106EB0_5254E0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106F50_525580);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80106FD8_525608);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107048_525678);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801070AC_5256DC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801070D8_525708);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107140_525770);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_8010738C_5259BC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801074D8_525B08);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107640_525C70);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801077E8_525E18);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801078B4_525EE4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107980_525FB0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107A4C_52607C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107A90_5260C0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107B74_5261A4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107BD8_526208);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107C08_526238);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107C70_5262A0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107CA0_5262D0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107CD0_526300);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107D9C_5263CC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107DD8_526408);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107E10_526440);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107E2C_52645C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107E80_5264B0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107EBC_5264EC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80107EF4_526524);
+
+void func_801080A8_5266D8() {
+    func_801061F0_524820();
+    func_80110F48_52F578();
+    func_80012508(0, &D_80114BB8_5331E8, &D_80114BC4_5331F4);
+    func_800124BC(0, &D_80114BD0_533200);
+    func_800E6630_FA250(&bin_hvq_ROM_START);
+    func_80112944_530F74();
+    func_80110B90_52F1C0();
+    func_80114A2C_53305C();
+}
+
+void func_80108118_526748(s32 arg1, s32 arg2) {
+    func_80107140_525770(arg1, arg2, 0xA0, 0xC2, 0, 0, 4);
+}
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80108148_526778);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110B3C_52F16C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110B90_52F1C0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110B9C_52F1CC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110C04_52F234);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110D8C_52F3BC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110DE8_52F418);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110E80_52F4B0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110F10_52F540);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110F2C_52F55C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110F48_52F578);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80110FB0_52F5E0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111028_52F658);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_8011105C_52F68C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111090_52F6C0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801110AC_52F6DC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801110F0_52F720);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111164_52F794);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801111AC_52F7DC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_8011124C_52F87C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111490_52FAC0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111580_52FBB0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801117D4_52FE04);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111984_52FFB4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111B34_530164);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111C08_530238);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111C30_530260);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111C88_5302B8);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111DDC_53040C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80111F58_530588);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80112120_530750);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_8011225C_53088C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801124B4_530AE4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801128E0_530F10);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80112944_530F74);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80112978_530FA8);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801129B0_530FE0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80112A10_531040);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80112A74_5310A4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80112BA0_5311D0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80112C8C_5312BC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80112D30_531360);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80112DD8_531408);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80112FE4_531614);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801130B8_5316E8);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80113240_531870);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80113314_531944);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80113418_531A48);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801134FC_531B2C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_8011361C_531C4C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_8011373C_531D6C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801139B0_531FE0);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80113AE8_532118);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80113E8C_5324BC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114150_532780);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114250_532880);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114384_5329B4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114464_532A94);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801144A8_532AD8);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114628_532C58);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_8011477C_532DAC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801147D4_532E04);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_8011487C_532EAC);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114924_532F54);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114964_532F94);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_801149A4_532FD4);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114A10_533040);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114A2C_53305C);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114AA8_5330D8);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114AD8_533108);
+
+INCLUDE_ASM(s32, "overlays/start_screen/524820", func_80114B08_533138);
