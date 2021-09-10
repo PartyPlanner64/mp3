cf7c4e0f64d10aea5f5ec0700b840026aff9c02c
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-09-10 16:12:43 -0500

    Split out 5486A0

---
+++ src/overlays/overlay124/5486A0.c
@@ -0,0 +1,52 @@
+#include "common.h"
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010BE20_5486A0);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010BF84_548804);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010C680_548F00);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010C8F4_549174);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010CD74_5495F4);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010CFD4_549854);
+
+// References hvq rom start.
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010D1E8_549A68);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010D450_549CD0);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010D580_549E00);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010D998_54A218);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010DE50_54A6D0);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010DEDC_54A75C);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010DEE4_54A764);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010DEF4_54A774);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010DF04_54A784);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010DF0C_54A78C);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010DF24_54A7A4);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010DF2C_54A7AC);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010DF34_54A7B4);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010F04C_54B8CC);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010F3E4_54BC64);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010F4C4_54BD44);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010F9D4_54C254);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010FB28_54C3A8);
+
+INCLUDE_ASM(s32, "overlays/overlay124/5486A0", func_8010FB88_54C408);
