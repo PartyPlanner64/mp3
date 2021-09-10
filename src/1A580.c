037fe7f3c25f148dd6f51b7be96c480276366140
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-09-10 15:50:04 -0500

    attempt func_8010FC98_4CCEC8

--- src/1A580.c
+++ src/1A580.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "1A580.h"
 
 // struct strCB8C0 {
 //     s16 unk0;
@@ -15,37 +16,6 @@
 extern s16 D_800CB8CE[];
 
 extern s16 D_800CDD6A;
-
-struct strD03F8 {
-    s8 unk0;
-    s8 unk1;
-    u8 unk2;
-    s8 unk3;
-    u8 unk4;
-    s8 unk5;
-    s8 unk6;
-    s8 unk7;
-    s16 unk8;
-    s16 unkA;
-    s16 unkC;
-    s16 unkE;
-    s8 unks10to40[0x30];
-
-    f32 unk40;
-    f32 unk44;
-    f32 unk48;
-    f32 unk4C;
-    f32 unk50;
-    f32 unk54;
-
-    s8 unks58toB0[0x58];
-
-    s32 unkB0;
-    struct strD03F8 *unkB4;
-    s32 unkB8;
-    s32 unkBC;
-}; // sizeof == 0xC0
-extern struct strD03F8 *D_800D03F8;
 
 extern void (*D_800D135C)(u8 *);
 extern s16 D_800D5204;
---
+++ src/overlays/overlay110/4C8050.c
@@ -0,0 +1,309 @@
+#include "common.h"
+#include "../../process.h"
+#include "../../object.h"
+#include "../../1A580.h"
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010AE20_4C8050);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010B124_4C8354);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010B9CC_4C8BFC);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010B9FC_4C8C2C);
+
+void func_8010BAF0_4C8D20(void) {
+}
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010BAF8_4C8D28);
+
+void func_8010BF3C_4C916C(void) {
+}
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010BF44_4C9174);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010C080_4C92B0);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010C0BC_4C92EC);
+
+void func_8010C54C_4C977C(void) {
+}
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010C554_4C9784);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010C590_4C97C0);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010C7F8_4C9A28);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010C8D0_4C9B00);
+
+void func_8010CAE8_4C9D18(void) {
+}
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010CAF0_4C9D20);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010CD40_4C9F70);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010CE90_4CA0C0);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010CFE0_4CA210);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010D128_4CA358);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010D1A0_4CA3D0);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010E5C4_4CB7F4, void *a, s32 b, f32 c, f32 d, s32 e, s32 f);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010E7C0_4CB9F0);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010EA34_4CBC64);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010EC38_4CBE68);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010EDA0_4CBFD0);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010EE3C_4CC06C);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010EF00_4CC130);
+
+INCLUDE_ASM(void, "overlays/overlay110/4C8050", func_8010EF28_4CC158, s32 a);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010EF50_4CC180);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010EF80_4CC1B0);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010F010_4CC240);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010F0A0_4CC2D0);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010F0E0_4CC310);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010F12C_4CC35C);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010F178_4CC3A8);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010F1F4_4CC424);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010F588_4CC7B8);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010F660_4CC890);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010F910_4CCB40);
+
+extern void func_8001247C(s32, f32, f32, f32); // extern
+extern void func_80017954(s16, s32, u8, s16); // extern
+extern void func_8001C0C4(s16, s32, s32, s32); // extern
+extern void func_8001C150(s16, s32, s32, s32); // extern
+extern void func_8001C1A0(s16, s32, s32, s32); // extern
+extern void func_8001C258(s16, s32, s32); // extern
+extern void func_8001C5B4(s16, s16); // extern
+extern void func_8001C624(s16, u8, s32, s32, s32); // extern
+extern void func_8001C814(s16, s32, s32); // extern
+extern void func_8004AC10(s32, u8); // extern
+extern void func_80055670(s16); // extern
+extern void func_800E4F50_CCD20(s32); // extern
+extern void func_800E5040_CCE10(s32, s32); // extern
+extern void func_80106C78_4C3EA8(); // extern
+extern void func_80106CA8_4C3ED8(); // extern
+extern void func_80106CCC_4C3EFC(s32); // extern
+extern void func_8010724C_4C447C(void *, s32, s32, s32, s32, s32); // extern
+extern void func_80107480_4C46B0(void *, s32, s32, s32, s32); // extern
+extern void func_801074AC_4C46DC(s16); // extern
+extern void func_8010EF28_4CC158(s32); // extern
+extern void func_80110DB0_4CDFE0(s32, f32, f32, s32); // extern
+extern void func_801118F8_4CEB28(s32, s32); // extern
+extern void func_8011196C_4CEB9C(s32); // extern
+extern u8 D_800D0309;
+extern s32 D_80111AC4_4CECF4;
+extern struct coords_3d D_80111AD0_4CED00;
+extern struct coords_3d D_80111ADC_4CED0C;
+extern s32 D_80111B8C_4CEDBC;
+
+struct strD_80112088_4CF2B8 {
+    s16 unk0[4];
+};
+extern struct strD_80112088_4CF2B8 D_80112088_4CF2B8;
+
+struct temp_sthings_unk40 {
+    s16 unk0;
+    s16 unk2;
+    s16 unk4;
+    s16 unk6;
+    s32 unk8;
+    s32 unkC;
+};
+struct temp_sthings_unk48 {
+    s16 unk0;
+    s16 unk2;
+    s16 unk4;
+    s16 unk6;
+    s16 unk8;
+    s16 unkA;
+    s32 unkC;
+    s32 unk10;
+    s16 unk14;
+    s16 unk16;
+    s16 unk18;
+    s16 unk1A;
+};
+struct temp_sthings {
+    s32 unk0[16];
+    struct temp_sthings_unk40 *unk40;
+    s32 unk44;
+    struct temp_sthings_unk48 *unk48;
+};
+
+struct strD_801122A0_282D70 {
+    struct temp_sthings *unk0;
+    s32 unk4;
+    s32 unk8;
+    s32 unkC;
+    s32 unk10;
+    s32 unk14;
+    struct temp_sthings *unk18;
+};
+extern struct strD_801122A0_282D70 D_801122A0_282D70;
+extern s16 D_80112330_282E00;
+extern s16 D_80112378_282E48[];
+extern s16 D_801123A4;
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_8010FC98_4CCEC8);
+#ifdef _NONMATCH
+void func_8010FC98_4CCEC8() {
+    s16 temp_s4;
+    s32 i;
+    s32 temp_s1_2;
+    s32 temp_s2;
+    s32 temp_s3;
+    u16 *temp_s0;
+    u16 temp_fp;
+    struct temp_sthings *temp_s5;
+    struct temp_sthings *temp_s7;
+    s32 phi_v0;
+    s32 phi_s0;
+    s32 phi_s1;
+    s32 phi_s2;
+    s32 phi_a3;
+    s32 phi_s1_2;
+    s32 phi_s3;
+    s32 phi_s3_2;
+
+    temp_s5 = D_801122A0_282D70.unk0;
+    temp_s7 = D_801122A0_282D70.unk18;
+    temp_fp = temp_s5->unk40->unk2;
+    func_8001247C(0, 40.0f, 80.0f, 8000.0f);
+    D_80111AD0_4CED00.x = 0;
+    D_80111AD0_4CED00.y = 190.0f;
+    D_80111AD0_4CED00.z = 3770.0f;
+    D_80111ADC_4CED0C.x = 0;
+    D_80111ADC_4CED0C.y = 130.0f;
+    D_80111ADC_4CED0C.z = 450.0f;
+    D_80111AC4_4CECF4 = 1;
+    func_800E4F50_CCD20(&bin_hvq_ROM_START);
+    func_800E5040_CCE10(0x20, 0);
+    func_8010EF28_4CC158(7);
+
+    for (i = 0; i < 6; i++) {
+        if (D_80112088_4CF2B8.unk0[0] > 0) {
+            temp_s4 = D_80112088_4CF2B8.unk0[0];
+            for (temp_s2 = 0; temp_s2 < temp_s4; temp_s2++) {
+                temp_s0 = D_80112378_282E48[temp_s2];
+                func_8000BD94(*temp_s0, 0, 0);
+                func_8000BDDC(*temp_s0, 1);
+                func_8000BB94(*temp_s0);
+                func_8000C184(*temp_s0);
+            }
+        }
+    }
+
+    func_80055670(D_801123A4);
+    func_8010F12C_4CC35C();
+    func_8001C5B4((s16) temp_s5->unk40->unk2, temp_s5->unk48->unk1A);
+    func_8001C814((s16) temp_s5->unk40->unk2, 2, 2);
+    func_8001C0C4((s16) temp_s5->unk40->unk2, 0, 0, 2220.0f);
+    func_8001C0C4(temp_s5->unk40->unk4, 0, -5.0f, 2220.0f);
+    func_8001C150((s16) temp_s5->unk40->unk2, 0, 0, 0);
+    func_8001C1A0((s16) temp_s5->unk40->unk2, 1.5f, 1.5f, 1.5f);
+    func_8001C1A0(temp_s5->unk40->unk4, 1.5f, 1.5f, 1.5f);
+    SleepVProcess();
+    func_8010F0A0_4CC2D0();
+    SleepProcess(0x1E);
+    func_8001C258(temp_s7->unk40->unkC, 4, 0);
+    func_8001C0C4(temp_s7->unk40->unkC, 0, 1000.0f, 2120.0f);
+    func_8001C150(temp_s7->unk40->unkC, 0, 0, 0);
+    func_8001C1A0(temp_s7->unk40->unkC, 1.5f, 1.5f, 1.5f);
+    func_8010E5C4_4CB7F4(temp_s7, 0, 400.0f, 2120.0f, 0x3C, 0);
+    D_80111B8C_4CEDBC = 0;
+    SleepProcess(0x14);
+    func_8001C624((s16) temp_fp, (temp_s5->unk48->unk0 + D_800D03F8)->unk2, 0, 0xA, 2);
+    func_80107480_4C46B0(temp_s5, 0, 180.0f, 0, 8);
+    SleepProcess(0x14);
+    func_80106C78_4C3EA8();
+    func_80106CCC_4C3EFC(0x5A7E);
+    func_80106CA8_4C3ED8();
+    func_8001C624((s16) temp_fp, (temp_s5->unk48->unk8 + D_800D03F8)->unk2, 0, 0xA, 0);
+    func_801074AC_4C46DC((s16) temp_fp);
+    func_8001C624((s16) temp_fp, (temp_s5->unk48->unk0 + D_800D03F8)->unk2, 0, 0xA, 2);
+    func_80107480_4C46B0(temp_s5, 0, 0, 0, 0xA);
+    func_8001C624((s16) temp_fp, (temp_s5->unk48->unk16 + D_800D03F8)->unk2, 0, 0xA, 2);
+    func_801118F8_4CEB28(0, 0x1F);
+    func_8010724C_4C447C(temp_s5, 0, 0, 2930.0f, 0x28, 0);
+    func_8011196C_4CEB9C(0);
+    func_8001C624((s16) temp_fp, (temp_s5->unk48->unk0 + D_800D03F8)->unk2, 0, 0xA, 2);
+    SleepProcess(0x14);
+    func_8004AC10(0x2C6, D_800D0309);
+    func_8001C624((s16) temp_fp, (temp_s5->unk48->unk4 + D_800D03F8)->unk2, 0, 0xA, 0);
+    func_80017954((s16) temp_fp, 2, D_800D0309, D_80112330_282E00);
+    func_8010E5C4_4CB7F4(temp_s7, 0, 400.0f, 2500.0f, 0x14, 0);
+    func_801074AC_4C46DC((s16) temp_fp);
+    func_8001C624((s16) temp_fp, (temp_s5->unk48->unk6 + D_800D03F8)->unk2, 0, 0xA, 2);
+    func_80110270_4CD4A0();
+    SleepVProcess();
+
+    for (i = 0; i < 4; i++) {
+        func_80110DB0_4CDFE0(0, 400.0f, 2500.0, i);
+        SleepProcess(2);
+    }
+}
+#endif
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110270_4CD4A0);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110394_4CD5C4);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110460_4CD690);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110494_4CD6C4);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_801104D4_4CD704);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110500_4CD730);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110528_4CD758);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110724_4CD954);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110888_4CDAB8);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_801108FC_4CDB2C);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_801109EC_4CDC1C);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110B4C_4CDD7C);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110BB4_4CDDE4);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110D5C_4CDF8C);
+
+INCLUDE_ASM(void, "overlays/overlay110/4C8050", func_80110DB0_4CDFE0, s32 a, f32 b, f32 c, s32 d);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80110E3C_4CE06C);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80111430_4CE660);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_80111740_4CE970);
+
+INCLUDE_ASM(void, "overlays/overlay110/4C8050", func_801118F8_4CEB28, s32 a, s32 b);
+
+INCLUDE_ASM(void, "overlays/overlay110/4C8050", func_8011196C_4CEB9C, s32 a);
+
+INCLUDE_ASM(s32, "overlays/overlay110/4C8050", func_801119C0_4CEBF0);
