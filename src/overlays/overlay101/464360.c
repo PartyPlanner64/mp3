6484d005d769918c11fd92d2bf17bf8c070800cc
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2021-09-09 22:03:09 -0500

    func_801088C4_467284

---
+++ src/overlays/overlay101/464360.c
@@ -0,0 +1,47 @@
+#include "common.h"
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_801059A0_464360);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80105B54_464514);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80105CD4_464694);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80105E38_4647F8);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80106000_4649C0);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_8010610C_464ACC);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_801061DC_464B9C);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80106224_464BE4);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_801062E0_464CA0);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80106808_4651C8);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_8010686C_46522C);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80106BF0_4655B0);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80108774_467134);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_801087B4_467174);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80108810_4671D0);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80108854_467214);
+
+INCLUDE_ASM(s32, "overlays/overlay101/464360", func_80108880_467240);
+
+void func_801088C4_467284() {
+    func_80012220(1);
+    func_80019514(1, 0xFF, 0xFF, 0xFF);
+    func_800E4F50_CCD20(&bin_hvq_ROM_START);
+    func_800E52DC_CD0AC(0x1E);
+}
+
+void func_80108908_4672C8() {
+    func_800E52F8_CD0C8();
+    func_800E5000_CCDD0();
+}
